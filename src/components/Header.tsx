bb73ec97a7116806e7a2936566060e6216dd4e8f
EvertonAlmeida <everton.o.almeida@outlook.com>
2022-01-17 09:53:23 -0300

    feat: finalizing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 
 export const Header = () => {
@@ -22,6 +22,7 @@ export const Header = () => {
 				DashGo
 				<Text as="span" ml="1" color="pink.500">.</Text>
 			</Text>
+
 			<Flex
 				as="label"
 				flex="1"
@@ -46,6 +47,45 @@ export const Header = () => {
 					name="name"
 				/>
 				<Icon as={RiSearchLine} fontSize="20" />
+			</Flex>
+
+			<Flex
+				aling="center"
+				ml="auto"
+			>
+				<HStack
+					spacing="8"
+					mx="8"
+					pr="8"
+					py="1"
+					color="gray.300"
+					borderRightWidth={1}
+					borderColor="gray.700"
+				>
+					<Icon as={RiNotificationLine} fontSize="20"/>
+					<Icon as={RiUserAddLine} fontSize="20"/>
+				</HStack>
+
+				<Flex
+					align="center"
+				>
+					<Box mr="4" textAlign="right">
+						<Text>Everton Almeida</Text>
+						<Text
+							color="gray.300"
+							fontSize="small"
+						>
+							contato@evertonalmeida.com
+						</Text>
+					</Box>
+
+					<Avatar
+						size="md"
+						name="Everton Almeida"
+						src="https://github.com/evertonalmeida.png"
+					/>
+
+				</Flex>
 			</Flex>
 		</Flex>
 	);
